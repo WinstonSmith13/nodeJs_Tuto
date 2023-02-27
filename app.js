008d12231df5df7e7833fea7060e33908aa2252d
WinstonSmith13 <krouk.medy@gmail.com>
2023-02-27 16:18:56 +0100

    tuto: working on Os modules.

--- app.js
+++ app.js
@@ -5,8 +5,24 @@
 
 log('medy');  */
 
-const path = require('path');
+/* const path = require('path');
 
 var pathObj = path.parse(__filename);
 
 console.log(pathObj);
+ */
+/* {
+    root: '/',
+    dir: '/Library/simplon/nuxt/nodeFirst',
+    base: 'app.js',
+    ext: '.js',
+    name: 'app'
+  } */
+
+  const os = require('os');
+
+  var freemem = os.freemem();
+  var totalmem = os.totalmem();
+
+  console.log(`Total memory : ${totalmem}`);
+  console.log(`Free memory : ${freemem}`);
